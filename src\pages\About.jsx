eaecfea40d3b0408db4bfd3b6991caf65d637d69
Alvin132QR <oa8929843@gmail.com>
2024-06-01 15:37:51 +0700

    Add files via upload

---
+++ src\pages\About.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default About;
+function About() {
+  return (
+    <div>
+      <h1>About Pages</h1>
+      <p>New Force React</p>
+    </div>
+  );
+}
---
+++ src\pages\Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default Home;
+
+function Home() {
+  return (
+    <div>
+      <h1>Home Pages</h1>
+      <a href="/about">About</a>
+    </div>
+  );
+}
